b862508f114533e17424b5770529a5c72ecbd84b
Campbell,Gabriel(SPLHZ) <gabriel.campbell@klm.com>
2018-11-12 13:13:38 +0100

    Multiple Databases

--- src/main/java/com/gabriel/multipledatabaseconnection/controller/MultipleDatabaseController.java
+++ src/main/java/com/gabriel/multipledatabaseconnection/controller/MultipleDatabaseController.java
@@ -1,25 +1,12 @@
 package com.gabriel.multipledatabaseconnection.controller;
 
-
-
 import com.gabriel.multipledatabaseconnection.model.Person;
 import com.gabriel.multipledatabaseconnection.repository.inmemory.PersonRepositoryInMemory;
 import com.gabriel.multipledatabaseconnection.repository.persistent.PersonRepositoryPersistent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 
 @EnableTransactionManagement
 @RestController
@@ -38,9 +25,7 @@ public class MultipleDatabaseController {
     @Transactional("inmemoryDatabaseTransactionManager")
     public Person findOneByIdNumber(@PathVariable("idNumber") Long idNumber) {
 
-
         Person result = personRepositoryInMemory.findFirstByIdNumber(idNumber);
-
 
         return new Person();
     }
