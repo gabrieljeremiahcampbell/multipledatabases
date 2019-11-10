875f6214ac105ba83d0c8125067fe3f3f9fe5b19
Gabriel J. Campbell <g.campbell@kcs.nl>
2019-11-10 23:39:35 +0100

    Logging AOP framework added.

---
+++ src/main/java/com/gabriel/logging/LoggingAOP.java
@@ -0,0 +1,40 @@
+package com.gabriel.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAOP {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        logger.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+
+    @Around("@annotation(LogExecutionDetails)")
+    public Object logExecutionDetails(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        logger.info("Signature="+joinPoint.getSignature() + ": Args= "+ joinPoint.getArgs() + ": Executed in " + executionTime + "ms");
+        return proceed;
+    }
+
+}
--- src/main/java/com/gabriel/multipledatabaseconnection/controller/MultipleDatabaseController.java
+++ src/main/java/com/gabriel/multipledatabaseconnection/controller/MultipleDatabaseController.java
@@ -1,8 +1,12 @@
 package com.gabriel.multipledatabaseconnection.controller;
 
+import com.gabriel.logging.LogExecutionDetails;
+import com.gabriel.logging.LogExecutionTime;
 import com.gabriel.multipledatabaseconnection.model.Person;
 import com.gabriel.multipledatabaseconnection.repository.inmemory.PersonRepositoryInMemory;
 import com.gabriel.multipledatabaseconnection.repository.persistent.PersonRepositoryPersistent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/person/")
 public class MultipleDatabaseController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private PersonRepositoryPersistent personRepositoryPersistent;
 
@@ -20,7 +26,7 @@ public class MultipleDatabaseController {
     private PersonRepositoryInMemory personRepositoryInMemory;
 
 
-
+    @LogExecutionDetails
     @RequestMapping(value = "{idNumber}" , method = RequestMethod.GET)
     @Transactional("inmemoryDatabaseTransactionManager")
     public Person findOneByIdNumber(@PathVariable("idNumber") Long idNumber) {
@@ -30,12 +36,15 @@ public class MultipleDatabaseController {
         return new Person();
     }
 
+    @LogExecutionTime
     @RequestMapping(value = "newperson" , method = RequestMethod.POST, consumes="application/json")
     public Person savePerson(@RequestBody Person person) {
 
+        logger.info("Hi");
         return savePersonPersistent(savePersonInMemory(person));
     }
 
+    @LogExecutionDetails
     @Transactional("persistentDatabaseTransactionManager")
     public Person savePersonPersistent(@RequestBody Person person) {
 
@@ -43,6 +52,7 @@ public class MultipleDatabaseController {
 
     }
 
+    @LogExecutionDetails
     @Transactional("inmemoryDatabaseTransactionManager")
     public Person savePersonInMemory(@RequestBody Person person) {
 
